b068f3da3f445062d5be2c3e4d8aa2713a88c52d
j0sa <jonas.arucard@gmail.com>
2020-06-09 20:39:21 +0200

    completed ex2

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -22,7 +22,10 @@ grad = zeros(size(theta));
 
 
 
+h_theta = sigmoid(X*theta);
+J = (1 / m) * ((-y' * log(h_theta)) - (1 - y)' * log(1 - h_theta));
 
+grad = (1 / m) * (h_theta - y)' * X;
 
 
 
--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -19,7 +19,13 @@ grad = zeros(size(theta));
 
 
 
+h_theta = sigmoid(X*theta);
+J = (1/m) * (-y' * log(h_theta) - (1-y)' * log(1-h_theta)) + (lambda/(2*m)) * (theta(2:length(theta)))' * theta(2:length(theta));
 
+thetaZero = theta;
+thetaZero(1) = 0;
+
+grad = ((1 / m) * (h_theta - y)' * X) + lambda / m * thetaZero';
 
 
 % =============================================================
